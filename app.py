f1c82d3445da53510c0869c0e3beedb06f1dfd4d
dtanaka-libersky <125267366+dtanaka-libersky@users.noreply.github.com>
2023-02-14 09:24:11 +0900

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,8 @@ def hello() -> str:
     # https://cloud.google.com/run/docs/logging#correlate-logs
     logger.info("Child logger with trace Id.")
 
-    return "Hello, World!"
+    #return "Hello, World!"
+    return "Step31 CloudRun Service"
 
 
 def shutdown_handler(signal_int: int, frame: FrameType) -> None:
